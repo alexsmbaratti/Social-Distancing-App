30ef2107bb7fda9b9b606169182bde8e19c5237b
Alex Baratti <alexsmbaratti@gmail.com>
2020-08-02 20:25:02 -0700

    Successfully renders the social distancing ring

--- Social Distance/ViewController.swift
+++ Social Distance/ViewController.swift
@@ -20,11 +20,24 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         arSceneView.session.run(ARWorldTrackingConfiguration()) // Track the world
+        renderSocialDistanceRing()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         arSceneView.session.pause() // Stop tracking world to save on resources
+    }
+    
+    func renderSocialDistanceRing() {
+        let ring = SCNTorus(ringRadius: 1.83, pipeRadius: 0.05)
+        let node = SCNNode()
+        let scene = SCNScene()
+        
+        node.geometry = ring
+        node.position = SCNVector3(0,0,-1.5)
+        
+        scene.rootNode.addChildNode(node)
+        arSceneView.scene = scene
     }
 
 }
