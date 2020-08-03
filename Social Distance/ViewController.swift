172e61f495fb439a1b09678c6ffe3fa17a6591d3
Alex Baratti <alexsmbaratti@gmail.com>
2020-08-02 20:14:17 -0700

    Adds ARKitSceneView and boilerplate methods

--- Social Distance/ViewController.swift
+++ Social Distance/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var arSceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        arSceneView.session.run(ARWorldTrackingConfiguration()) // Track the world
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        arSceneView.session.pause() // Stop tracking world to save on resources
+    }
 
 }
 
