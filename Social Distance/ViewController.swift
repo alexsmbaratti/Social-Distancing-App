380501e55c30d0cc4eadbdfb759f4030ae6c90ca
Alex Baratti <alexsmbaratti@gmail.com>
2020-08-02 20:05:57 -0700

    Initial Commit

---
+++ Social Distance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Social Distance
+//
+//  Created by Alex Baratti on 8/2/20.
+//  Copyright © 2020 Alex Baratti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
